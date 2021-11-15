d73dd8c5491055cd16563658c8cdf8c3ff54f46c
codecaiine <akabrouyannickn@gmail.com>
2021-11-15 23:38:32 +0000

    Add @specialization and a parameter for it

--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,7 @@
 class Teacher < Person
     # contructor
     def initialize(specialization, name = 'unknown', parent_permission: true)
+        super(name, age, parent_permission)
+        @specialization = specialization
     end
 end
