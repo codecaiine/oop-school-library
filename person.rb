7d6ccc7dbeb5a522ae5bc20840bda1632e0fea00
codecaiine <akabrouyannickn@gmail.com>
2021-11-15 21:25:09 +0000

    Create age method with condition

--- person.rb
+++ person.rb
@@ -5,11 +5,14 @@ class Person
 
     # constructor
     def initialize(name = 'Unknow', age, parent_permission: true)
-
         @id = Random.rand(1..1000)
         @name = name
         @age = age
         @parent_permission = parent_permission 
     end 
+   
+    def of_age?
+        @age >= 18
+    end
     
 end
