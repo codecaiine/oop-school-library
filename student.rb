975ba3f069da754303ea5c56a13c22e4cc66d143
codecaiine <akabrouyannickn@gmail.com>
2021-11-15 22:32:54 +0000

    Add @classroom and a parameter for it

--- student.rb
+++ student.rb
@@ -1,6 +1,7 @@
 class Student < Person
     # constructor
     def initialize(age, classroom, name = 'Unknown', parent_permission = true)
-        
+        super(name, age, parent_permission)
+        @classroom = classroom
     end 
 end
