99976dbcde7a503c867bf6b37c656e2ed67ceef3
codecaiine <akabrouyannickn@gmail.com>
2021-11-17 10:14:35 +0000

    Create has-many Book

--- book.rb
+++ book.rb
@@ -1,8 +1,13 @@
 class Book
     attr_accessor :author, :title
+    attr_reader :rentals
 
     def initialize(author, title)
-        @author: author
-        @title: title
+        @author = author
+        @title = title
+        @rentals = []
     end
+
+    def add_rental(rental)
+        @rentals.push(rental)
 end
--- rental.rb
+++ rental.rb
@@ -2,6 +2,6 @@ class Rental
     attr_accessor :date
 
     def initialize(date)
-      @date: date
+      @date = date
     end
 end
