45c589323cb148c0884022ca24767d6ded5fe994
codecaiine <akabrouyannickn@gmail.com>
2021-11-17 20:06:42 +0000

    Fix linters errors

--- person.rb
+++ person.rb
@@ -6,7 +6,7 @@ class Person
   attr_reader :id
 
   # constructor
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknow', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
