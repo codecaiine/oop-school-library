0796bfb6f203801c64362cb32ef18dbad35f1c7e
codecaiine <akabrouyannickn@gmail.com>
2021-11-18 15:18:05 +0000

    Initialize my class app

--- main.rb
+++ main.rb
@@ -6,4 +6,10 @@ require './book'
 require './rental'
 
 class App
+    def initialize
+        @people = []
+        @books = []
+        @rentals = []
+        @class = Classroom.new('Grade 5')
+    end
 end
