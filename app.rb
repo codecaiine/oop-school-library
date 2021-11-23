fed0ce80dba3e12e1b659846964c29673b18f945
codecaiine <akabrouyannickn@gmail.com>
2021-11-23 00:24:59 +0000

    Create display_books method in app.rb

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
 class App
 
+
+    def display_books
+        puts 'List of books:'
+        puts 'EMPTY' if @books.empty?
+        @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
+    end
+
 end
