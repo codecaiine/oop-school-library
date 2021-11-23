2c827ed2801a4403af5d3182c755b5ba45257183
codecaiine <akabrouyannickn@gmail.com>
2021-11-23 00:32:40 +0000

    Add display_people method in app.rb

--- app.rb
+++ app.rb
@@ -7,4 +7,12 @@ class App
         @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
     end
 
+    def display_people
+        puts 'List of people:'
+        puts 'EMPTY' if @people.empty?
+        @people.each_with_index do |person, index|
+          puts "#{index}) Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
+        end
+    end
+
 end
