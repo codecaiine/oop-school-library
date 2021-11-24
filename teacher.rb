58764078af72edb7b6577f84743dd29f704750c5
codecaiine <akabrouyannickn@gmail.com>
2021-11-24 15:12:00 +0000

    Serialize teacher data

--- teacher.rb
+++ teacher.rb
@@ -9,4 +9,15 @@ class Teacher < Person
   def can_use_services?
     true
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+       'id' => @id,
+       'age' => @age,
+       'name' => @name,
+       'specialization' => @specialization,
+       'parent_permission' => @parent_permission
+    }.to_json(*args)
+  end
 end
