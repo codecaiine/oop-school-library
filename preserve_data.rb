d562ea6dd59515974ca5d619a78813cb4177abf5
Kossi-stack <81444338+Kossi-stack@users.noreply.github.com>
2021-11-24 21:45:05 -0800

    Update JSON files

--- preserve_data.rb
+++ preserve_data.rb
@@ -6,7 +6,11 @@ require_relative 'person'
 require_relative 'rental'
 
 class Storage
-  
+  def stringify_data(person, book, rental)
+    File.open('books.json', 'w') { |f| f.write JSON.generate(book) }
+    File.open('persons.json', 'w') { |f| f.write JSON.generate(person) }
+    File.open('rentals.json', 'w') { |f| f.write JSON.generate(rental) }
+  end
 
   def parse
     {
